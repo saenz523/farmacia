6618ee67fcea590047b45ba82789ac1630845d58
Gustavo <saenz523@hotmail.com>
2024-09-21 23:28:07 -0500

    Funcionalidad Notificacion de agregar al carrito

--- scripts.js
+++ scripts.js
@@ -1,4 +1,20 @@
-buttonDark = document.getElementById('mode-dark')
+buttonDark = document.getElementById("mode-dark");
 buttonDark.addEventListener("click", (event) => {
-    document.body.classList.toggle('dark')
+  document.body.classList.toggle("dark");
 });
+
+let timeout;
+notification = document.getElementById("notification");
+buttonProducts = document.getElementsByClassName("btn-buy");
+for (let i = 0; i < buttonProducts.length; i++) {
+  buttonProducts[i].addEventListener("click", (event) => {
+    notification.classList.remove("close");
+    notification.classList.remove("closed");
+    if (timeout) {
+      clearTimeout(timeout);
+    }
+    timeout = setTimeout(() => {
+      notification.classList.add("close");
+    }, 3000);
+  });
+}
